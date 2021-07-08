8ff2ce8bd5536bc1fb1487ef2cfb418d555df191
Akshata Deshpande <ad.march6@gmail.com>
2021-07-08 21:14:48 +0100

    Initial Commit

---
+++ EmployeeDirectory/app/src/main/java/com/example/employeeandroomapp/di/ActivityBuildersModule.kt
@@ -0,0 +1,11 @@
+package com.example.employeeandroomapp.di
+
+import com.example.employeeandroomapp.ui.MainActivity
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+@Module
+abstract class ActivityBuildersModule {
+    @ContributesAndroidInjector(modules = [MainFragmentsBuildersModule::class])
+    abstract fun contributeMainActivity(): MainActivity
+}
